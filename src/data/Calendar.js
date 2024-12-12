e543abe27dded7c189f39178d6f45ab3838a4652
deokbin <deokbin@naver.com>
2024-12-12 20:03:43 +0900

    feat(Calendar): 달력 데이터, 요일, 법정공휴일 데이터 추가

---
+++ src/data/Calendar.js
@@ -0,0 +1,29 @@
+export const monthData = {
+  1: 31,
+  2: 28,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31,
+};
+
+export const daysOfWeek = ['월', '화', '수', '목', '금', '토', '일'];
+
+export const holidaysByMonth = {
+  1: [{ date: 1, name: '신정' }],
+  3: [{ date: 1, name: '삼일절' }],
+  5: [{ date: 5, name: '어린이날' }],
+  6: [{ date: 6, name: '현충일' }],
+  8: [{ date: 15, name: '광복절' }],
+  10: [
+    { date: 3, name: '개천절' },
+    { date: 9, name: '한글날' },
+  ],
+  12: [{ date: 25, name: '성탄절' }],
+};
