5aecb501aafe2c9edb27e0610339ae4000738ef3
deokbin <deokbin@naver.com>
2024-12-12 16:03:46 +0900

    feat(IOMessage): 입출력 메시지 상수 추가

---
+++ src/Constants/IOMessage.js
@@ -0,0 +1,6 @@
+const IOMessage = Object.freeze({
+  getMonthStartDay: `비상 근무를 배정할 월과 시작 요일을 입력하세요> `,
+  weekDayWorkOrder: `평일 비상 근무 순번대로 사원 닉네임을 입력하세요> `,
+  weekEndWorkOrder: `휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> `,
+});
+export default IOMessage;
