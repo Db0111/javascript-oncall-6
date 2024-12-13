0823bed332532e91ce468c748fd9d4854fe7ca3f
deokbin <deokbin@naver.com>
2024-12-13 14:00:44 +0900

    refactor(Controller): 컨트롤러 로직 변경

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -1,6 +1,7 @@
 import IOMessage from '../Constants/IOMessage.js';
 import EmergencyService from '../Model/EmergencyService.js';
 import Month from '../Model/Month.js';
+import SchedulePrinter from '../Model/SchedulePrinter.js';
 import { InputView } from '../View/inputView.js';
 import { OutputView } from '../View/outputView.js';
 import InputValidator from '../utils/InputValidator.js';
@@ -9,24 +10,26 @@ import splitInput from '../utils/splitInput.js';
 
 class Controller {
   static async arrangeEmergencyWork() {
-    const monthInput = await this.getMonthStartDay();
-    const { month, startDay } = EmergencyService.createMonthData(monthInput);
+    const { month, startDay } = await this.getMonthStartDay();
     const monthInstance = new Month(month, startDay);
     const calendar = monthInstance.makeCalendar(month, startDay);
     const { weekDayWorkerList, weekEndWorkerList } = await this.getWorkerList();
-    const schedule = EmergencyService.arrangeSchedule(weekDayWorkerList, weekEndWorkerList, calendar);
-    OutputView.printMessage(schedule);
 
-    // 휴일 비상근무 순번 입력 받기
-    // 편성
-    // 편성 결과 출력
+    const emergencyService = new EmergencyService();
+
+    const schedule = SchedulePrinter.printSchedule(month, emergencyService.arrangeSchedule(weekDayWorkerList, weekEndWorkerList, calendar));
+
+    OutputView.printMessage(schedule);
   }
 
   static async getMonthStartDay() {
     while (true) {
       try {
         const userInput = await InputView.getInput(IOMessage.getMonthStartDay);
-        return userInput;
+        const month = Number(splitInput(userInput)[0]);
+        const startDay = splitInput(userInput)[1];
+        InputValidator.isValid(month, startDay);
+        return { month, startDay };
       } catch (error) {
         OutputView.printMessage(error.message);
       }
@@ -34,14 +37,16 @@ class Controller {
   }
 
   static async getWorkerList() {
-    try {
-      const getWeekDayWorker = await InputView.getInput(IOMessage.weekDayWorkOrder);
-      const weekDayWorkerList = WorkerValidator.isValid(splitInput(getWeekDayWorker));
-      const getWeekendWorker = await InputView.getInput(IOMessage.weekEndWorkOrder);
-      const weekEndWorkerList = WorkerValidator.isValid(splitInput(getWeekendWorker));
-      return { weekDayWorkerList, weekEndWorkerList };
-    } catch (error) {
-      OutputView.printMessage(error.message);
+    while (true) {
+      try {
+        const getWeekDayWorker = await InputView.getInput(IOMessage.weekDayWorkOrder);
+        const weekDayWorkerList = WorkerValidator.isValid(splitInput(getWeekDayWorker));
+        const getWeekendWorker = await InputView.getInput(IOMessage.weekEndWorkOrder);
+        const weekEndWorkerList = WorkerValidator.isValid(splitInput(getWeekendWorker));
+        return { weekDayWorkerList, weekEndWorkerList };
+      } catch (error) {
+        OutputView.printMessage(error.message);
+      }
     }
   }
 }
