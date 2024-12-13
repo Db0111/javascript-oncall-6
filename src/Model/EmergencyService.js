ab41691be23d70683fcb3040727268ece12f1abd
deokbin <deokbin@naver.com>
2024-12-13 14:00:26 +0900

    fix(EmergencyService): 비상 근무 인덱스 인스턴스로 관리

--- src/Model/EmergencyService.js
+++ src/Model/EmergencyService.js
@@ -2,21 +2,24 @@ import splitInput from '../utils/splitInput.js';
 import InputValidator from '../utils/InputValidator.js';
 
 class EmergencyService {
-  static weekDayIndex = 0;
-  static weekEndIndex = 0;
+  constructor() {
+    this.weekDayIndex = 0;
+    this.weekEndIndex = 0;
+  }
 
   static createMonthData(userInput) {
     const month = Number(splitInput(userInput)[0]);
     const startDay = splitInput(userInput)[1];
+
     InputValidator.isValid(month, startDay);
+
     return { month, startDay };
   }
 
-  static arrangeSchedule(weekDayWorkerList, weekEndWorkerList, calendar) {
+  arrangeSchedule(weekDayWorkerList, weekEndWorkerList, calendar) {
     let workerSchedule = {};
     for (let date in calendar) {
       const dayData = calendar[date];
-      console.log(dayData);
       let worker = null;
       if (!dayData.isHoliday && !dayData.isWeekend) {
         this.arrangeWeekday(date, dayData, worker, weekDayWorkerList, workerSchedule);
@@ -28,7 +31,7 @@ class EmergencyService {
     return workerSchedule;
   }
 
-  static arrangeWeekday(day, dayData, worker, weekDayWorkerList, workerSchedule) {
+  arrangeWeekday(day, dayData, worker, weekDayWorkerList, workerSchedule) {
     worker = weekDayWorkerList[this.weekDayIndex];
     if (workerSchedule[day - 1] && workerSchedule[day - 1].worker === worker) {
       this.weekDayIndex = (this.weekDayIndex + 1) % weekDayWorkerList.length;
@@ -38,11 +41,9 @@ class EmergencyService {
     this.weekDayIndex = (this.weekDayIndex + 1) % weekDayWorkerList.length;
   }
 
-  static arrangeWeekend(day, dayData, worker, weekEndWorkerList, workerSchedule) {
+  arrangeWeekend(day, dayData, worker, weekEndWorkerList, workerSchedule) {
     worker = weekEndWorkerList[this.weekEndIndex];
-    // 연속 근무 방지 처리
     if (workerSchedule[day - 1] && workerSchedule[day - 1].worker === worker) {
-      // 순서 변경
       this.weekEndIndex = (this.weekEndIndex + 1) % weekEndWorkerList.length;
       worker = weekEndWorkerList[this.weekEndIndex];
     }
